57b96c2a89aed822a49c0c4b86f72dd44554a858
Christopher Speck <die.drachen@gmail.com>
2023-05-20 00:42:21 -0400

    Fixes for moving lines up/down, ignore compiled lua file

--- lua/speck/remap.lua
+++ lua/speck/remap.lua
@@ -2,10 +2,14 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- Easy way to move selected lines up/down
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m'>+<CR>gv=gv")
+vim.keymap.set("v", "K", ":m-2<CR>gv=gv")
 
--- Keep cursor position when using J 
+vim.keymap.set("n", "<C-j>", ":<C-u>m+<CR>==")
+vim.keymap.set("n", "<C-k>", ":<C-u>m-2<CR>==")
+
+
+-- Keep cursor position when using J
 --vim.keymap.set("n", "J", "mzJ`z")
 
 -- Keep cursor in middle of screen when using C-d/C-u
@@ -38,7 +42,7 @@ vim.keymap.set("n", "Q", "<nop>")
 -- Format entire file
 vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format() end)
 
--- 
+--
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
