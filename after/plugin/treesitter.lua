b8714937a748a82fd1a11eca8a3358c4558af09d
Christopher Speck <die.drachen@gmail.com>
2023-11-25 15:15:11 -0500

    Configure treesitter-context

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -53,5 +53,30 @@ require('nvim-treesitter.configs').setup {
         enable = true,
     },
 
-
 }
+
+local ts_ctx = require('treesitter-context')
+ts_ctx.setup {
+    enable = true,
+    -- Only show a max of 5 lines of context/scope otherwise it's overwhelming
+    -- and hides contextually-relevant code near the cursor.
+    max_lines = 5,
+    -- When max_lines is reached trim the inner scope and show the outer scope,
+    -- as it's more likely the inner scope is more easily accessible through
+    -- navigation.
+    trim_scope = 'inner',
+    -- Either 'cursor' or 'topline', specifies which to use for relative scope.
+    -- Using 'cursor' causes the context to change while moving around, which
+    -- is distracting.
+    mode = 'topline',
+
+    on_attach = function(buf)
+        -- Disable attaching to prompts or non-file buffers.
+        local buftype = vim.bo.buftype
+        if buftype == 'prompt' or buftype == 'nofile' then
+            return false
+        end
+    end,
+}
+
+vim.keymap.set("n", "gc", function() ts_ctx.go_to_context() end, { silent = true })
