6420d25c8238544493b5d77ae70e8713c6cae140
Christopher Speck <die.drachen@gmail.com>
2023-12-27 12:45:39 -0500

    Handle formatting in visual mode separately

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -111,21 +111,27 @@ lsp.setup_nvim_cmp({
 lsp.on_attach(function(client, bufnr)
     local opts = { buffer = bufnr, remap = false }
 
-    local ts = require('telescope.builtin')
+    local telescope = require('telescope.builtin')
 
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
     vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
     vim.keymap.set("n", "go", function() vim.lsp.buf.type_definition() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set("n", "<leader>fS", function() ts.lsp_dynamic_workspace_symbols() end, opts)
+    vim.keymap.set("n", "<leader>fS", function() telescope.lsp_dynamic_workspace_symbols() end, opts)
     vim.keymap.set("n", "<leader>sd", function() vim.diagnostic.open_float() end, opts)
     vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
     vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
     vim.keymap.set("n", "<leader>sa", function() vim.lsp.buf.code_action() end, opts)
     vim.keymap.set("n", "<leader>lR", function() vim.lsp.buf.references() end, opts)
     vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
-    vim.keymap.set({ "n", "v" }, "<leader>rf", function() vim.lsp.buf.format() end, opts)
+    vim.keymap.set("n", "<leader>rf", function() vim.lsp.buf.format() end, opts)
+    vim.keymap.set("v", "<leader>rf", function()
+        local range = {}
+        range['start'] = vim.fn.getpos('v')
+        range['end'] = vim.fn.getpos('.')
+        vim.lsp.buf.format({ range = range })
+    end, opts)
     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
