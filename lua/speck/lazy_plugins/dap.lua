03e8d3a6e7f21dbc34ba4c7601ef0a049b000e3c
Christopher Speck <die.drachen@gmail.com>
2024-01-21 21:09:02 -0500

    chore: update deps

---
+++ lua/speck/lazy_plugins/dap.lua
@@ -0,0 +1,15 @@
+-- Debug Adapter Protocol
+
+--[[
+return {
+    'mfussenegger/nvim-dap',
+    config = function()
+        local dap = require('dap')
+        dap.setup()
+
+
+    end,
+}
+]]--
+
+return {}
