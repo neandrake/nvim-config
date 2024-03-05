e26773f9055a71f6e823b30b1eea58538ff16c7f
Christopher Speck <cspeckrun@gmail.com>
2024-03-05 08:10:01 -0500

    fix: specify valid branch for lazy.nvim, update plugins

--- lua/speck/lazy.lua
+++ lua/speck/lazy.lua
@@ -3,7 +3,7 @@ local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
     local repo = 'https://github.com/folke/lazy.nvim.git'
     vim.fn.system({
-        'git', 'clone', '--filter=blob:none', repo, '--branch=stable', lazypath
+        'git', 'clone', '--filter=blob:none', repo, '--branch=main', lazypath
     })
 end
 -- Ensure it's part of the runtime path.
