cb1568e7ef6bd8e8129ca2e59f20629c9b64cce8
Christopher Speck <cspeckrun@gmail.com>
2024-05-04 20:52:18 -0400

    maint: make code actions available in visual mode, update plugins

--- lua/speck/lazy_plugins/lsp.lua
+++ lua/speck/lazy_plugins/lsp.lua
@@ -253,7 +253,7 @@ return {
                 vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
                 vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
                 vim.keymap.set("n", "<leader>gr", function() vim.lsp.buf.references() end, opts)
-                vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+                vim.keymap.set({"n", "v"}, "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
                 vim.keymap.set("n", "<leader>cn", function() vim.lsp.buf.rename() end, opts)
                 vim.keymap.set("n", "<leader>cf", function() vim.lsp.buf.format() end, opts)
                 vim.keymap.set("v", "<leader>cf", function()
