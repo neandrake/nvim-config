a620c68bf156812590edd424b2ab7f16039a056e
Christopher Speck <cspeckrun@gmail.com>
2024-06-14 01:46:11 -0400

    fix: migrate trouble config after v3.0 upgrade

--- lua/speck/lazy_plugins/trouble.lua
+++ lua/speck/lazy_plugins/trouble.lua
@@ -6,31 +6,41 @@ return {
         'nvim-tree/nvim-web-devicons',
     },
 
-    config = function()
-        local trouble = require('trouble')
+    cmd = "Trouble",
+    opts = {
+        mode = "my_default",
+        multiline = true,    -- Render multi-line messages.
+        auto_open = false,   -- Automatically open the list when there are diagnostics.
+        auto_close = false,  -- Automatically close the list when there are no diagnostics.
+        auto_preview = true, -- Automatically preview the location of the diagnostic.
 
-        trouble.setup {
-            position = 'bottom',
-            height = 10,
-            icons = true, -- Use 'nvim-web-devicons'
-            mode = 'workspace_diagnostics',
-            severity = nil, -- Use nil = ALL, otherwise vim.diagnostic.severity.ERROR | WARN | INFO | HINT.
-            group = true, -- Group diagnostics by file.
-            multiline = true, -- Render multi-line messages.
-            auto_open = false, -- Automatically open the list when there are diagnostics.
-            auto_close = false, -- Automatically close the list when there are no diagnostics.
-            auto_preview = true, -- Automatically preview the location of the diagnostic.
-            auto_fold = true, -- Automatically fold a file trouble list at creation.
+        -- Automatically jump if there's only a single result.
+        auto_jump = true,
 
-            -- For given modes, automatically jump if there's only a single result.
-            auto_jump = { 'lsp_definitions' },
+        -- Define custom "modes".
+        modes = {
+            my_default = {
+                mode = "diagnostics",
+                preview = {
+                    type = "split",
+                    relative = "win",
+                    position = "right",
+                    size = 0.3,
+                },
+            },
+        },
+    },
 
-            -- For given modes, include the declaration of the current symbol in the results.
-            include_declaration = { 'lsp_references', 'lsp_implementations', 'lsp_definitions' },
-
-            use_diagnostic_signs = true, -- Icons are defined for lsp via 'lspkind' plugin, use those.
-        }
-
-        vim.keymap.set("n", "<leader>td", trouble.toggle)
-    end
+    keys = {
+        {
+            "<leader>td",
+            "<cmd>Trouble diagnostics toggle<cr>",
+            desc = "Diagnostics (Trouble)",
+        },
+        {
+            "<leader>tD",
+            "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+            desc = "Buffer Diagnostics (Trouble)",
+        },
+    },
 }
