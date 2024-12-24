b91892f8a5f75512417087af67a0a8fbed1a78a6
Christopher Speck <cspeckrun@gmail.com>
2024-12-23 21:38:46 -0500

    maint: looking at another color scheme, everforest

--- lua/speck/lazy_plugins/theme.lua
+++ lua/speck/lazy_plugins/theme.lua
@@ -4,9 +4,23 @@ return {
 
     config = function()
         require('gruvbox').setup {
-            contrast = "hard",
+            --contrast = "hard",
         }
 
-        vim.cmd('colorscheme gruvbox')
+        vim.cmd.colorscheme('gruvbox')
     end
 }
+
+--[[
+return {
+    'sainnhe/everforest',
+    lazy = false,
+    priority = 1000,
+    config = function()
+        vim.g.everforest_enable_italic = true
+        vim.g.everforest_background = 'soft'
+        vim.cmd.colorscheme('everforest')
+    end
+}
+]]--
+
