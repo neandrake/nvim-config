9388f21677b7a1459d91aeaa22042a5f9534ff1f
Christopher Speck <cspeckrun@gmail.com>
2025-08-30 17:30:31 -0400

    fix: update trouble config and fix it to actually be used, update plugins

--- lua/speck/lazy_plugins/trouble.lua
+++ lua/speck/lazy_plugins/trouble.lua
@@ -9,7 +9,7 @@ return {
     cmd = "Trouble",
     opts = {
         mode = "my_default",
-        multiline = true,    -- Render multi-line messages.
+        multiline = false,   -- Do not render multi-line messages.
         auto_open = false,   -- Automatically open the list when there are diagnostics.
         auto_close = false,  -- Automatically close the list when there are no diagnostics.
         auto_preview = true, -- Automatically preview the location of the diagnostic.
@@ -19,14 +19,26 @@ return {
 
         -- Define custom "modes".
         modes = {
-            my_default = {
+            my_mode = {
+                -- Inherit from the built-in 'diagnostics' mode.
                 mode = "diagnostics",
+                -- Embed the preview on the right-side of the trouble pane.
                 preview = {
                     type = "split",
                     relative = "win",
                     position = "right",
                     size = 0.3,
                 },
+                -- Filter out diagnostics by severity, so they won't show intermixed.
+                filter = function(items)
+                    local severity = vim.diagnostic.severity.HINT
+                    for _, item in ipairs(items) do
+                        severity = math.min(severity, item.severity)
+                    end
+                    return vim.tbl_filter(function(item)
+                        return item.severity == severity
+                    end, items)
+                end,
             },
         },
     },
@@ -34,12 +46,12 @@ return {
     keys = {
         {
             "<leader>td",
-            "<cmd>Trouble diagnostics toggle<cr>",
+            "<cmd>Trouble diagnostics toggle mode=my_mode<cr>",
             desc = "Diagnostics (Trouble)",
         },
         {
             "<leader>tD",
-            "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+            "<cmd>Trouble diagnostics toggle mode=my_mode filter.buf=0<cr>",
             desc = "Buffer Diagnostics (Trouble)",
         },
     },
